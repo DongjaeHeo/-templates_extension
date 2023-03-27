2189de5370340eae3a870f3ca8a0f7a6f6ad6016
Eloise Pividori <eloisepividori@gmail.com>
2023-03-27 17:45:13 +0200

    workspace styling

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,12 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["list"];
+  static targets = ["list", "options"];
   connect() {
     console.log("yay");
   }
 
   open_list(event) {
     this.listTarget.classList.toggle("d-none");
+  }
+
+  open_options(event) {
+    this.optionsTarget.classList.toggle("d-none");
   }
 }
